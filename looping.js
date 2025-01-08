24ca74d6629fd487070fc45c1556ed0998e7cffb
parsanithin5 <pparsanithin@gmail.com>
2025-01-08 13:18:29 +0530

    new file added

---
+++ looping.js
@@ -0,0 +1,45 @@
+ //for(let i=1;i<=5;i++){
+   //  console.log("javascript");
+    
+ //}
+
+ // for(let i=1;i<=100;i++){
+ //   if(i%2==0){
+//     console.log(i+ " is an even number");
+    
+ //   }else{
+ //     console.log(i+ " is an odd number");
+    
+ //   }
+    
+ // }
+
+
+
+ 
+
+// let mul=1
+// let num = Number(prompt("enter a number"));
+// for(let i=1;i<=num;i++){
+//     //console.log(i);
+//     mul = mul*i;
+    
+// }
+// console.log(mul);
+
+//while loop
+// let i = 1;
+// while(i<=5){
+//     console.log("hello");
+//     i++;
+// }
+
+// do-while loop
+
+let i =1;
+do{
+    i++
+    console.log("hello nithin");
+    
+}while(i<=5);
+
