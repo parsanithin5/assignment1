ca0c6e9505ef91cef07981f29fd8d3baf699febf
parsanithin5 <pparsanithin@gmail.com>
2025-01-24 19:28:31 +0530

    dom files

---
+++ dom.js
@@ -0,0 +1,36 @@
+// let element = document.getElementById("demo");
+// element.innerHTML = "DOM"
+// console.log(element);
+
+
+//let test = document.getElementById("test");
+// test.innerText = <h1>"Header"</h1>
+//console.log(test);
+
+
+//  let ele = document.getElementsByClassName("test");
+//  ele[0].style.backgroundColor="dodgerblue"
+// console.log(ele);
+// console.log(Array.isArray(ele));
+
+
+// let x =[...ele];
+// console.log(x, Array.isArray(x));
+// x.map(element=>{
+//     //console.log(element);
+//     element.style.backgroundColor="tomato";
+//     element.style.color="white";
+//     element.style.textTransform = "uppercase";
+    
+// })
+
+
+// let ele = document.querySelector("#demo");
+// console.log(ele);
+// let ele = document.querySelector(".test");
+// console.log(ele);
+
+
+let ele = document.querySelectorAll(".test");
+console.log(ele);
+
