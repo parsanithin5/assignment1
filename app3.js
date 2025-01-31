ed171f2c211c39ff4f454aaff2117461fe7dce00
parsanithin5 <pparsanithin@gmail.com>
2025-01-31 11:50:31 +0530

    modules

---
+++ app3.js
@@ -0,0 +1,19 @@
+// import x from "./child1.js"
+// console.log(x);
+
+// named import
+
+import college ,{x , user , users , obj} from "./child1.js"
+console.log(college);
+console.log(x);
+console.log(user);
+
+
+console.log(users);
+users.map(users=>{
+    console.log(user);
+})
+console.log(obj.company);
+
+
+
---
+++ child1.js
@@ -0,0 +1,39 @@
+
+
+// let x = 10;
+// export default x;
+// let userDetail={
+//     name:"nithin",
+//     profession:"student"
+// }
+// export default userDetails;
+
+//!named export
+   
+// let x = 10;
+// const user = "nithin";
+// const users =["srijesh","vardhan","manohar","sathwik","bhargav"];
+// const obj ={
+//     name:"chaithu",
+//     company:"Excelr"
+// }
+// export{
+//     x,
+//     user,
+//     users,
+//     obj
+// }
+
+export let x = 10;
+export const user="chaithu";
+
+export const users =["srijesh","vardhan","manohar","sathwik","bhargav"];
+
+export const obj={
+    name:"chaithu",
+    company:"Excelr"
+
+}
+
+let college="MallaReddy University";
+export default college;
